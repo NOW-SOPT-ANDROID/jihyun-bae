da1ccb99927a74eca9cdadfb092c3bbb0e5f9256
jihyunniiii <jita1125@naver.com>
2024-04-05 07:12:15 +0900

    [feat] #1 SoptEditText attrs

--- core-designsystem/src/main/java/com/sopt/now/component/SoptEditText.kt
+++ core-designsystem/src/main/java/com/sopt/now/component/SoptEditText.kt
@@ -2,9 +2,11 @@ package com.sopt.now.component
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import org.sopt.now.designsystem.R
 import org.sopt.now.designsystem.databinding.EditTextSoptBinding
 @SuppressLint("CustomViewStyleable")
 class SoptEditText @JvmOverloads constructor(
@@ -17,5 +19,20 @@ class SoptEditText @JvmOverloads constructor(
 
     init {
         binding = EditTextSoptBinding.inflate(LayoutInflater.from(context), this, true)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.SoptEditText)
+        try {
+            initView(typedArray)
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+    private fun initView(typedArray: TypedArray) {
+        typedArray.apply {
+            with(binding) {
+                tvEditTextSoptTitle.text = getString(R.styleable.SoptEditText_soptEditTextTitle)
+                etEditTextSoptEditText.hint = getString(R.styleable.SoptEditText_soptEditTextHint)
+            }
+        }
     }
 }
