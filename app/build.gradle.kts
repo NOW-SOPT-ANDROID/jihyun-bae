c6c4b6c1dc733aba2156b5e957ad0ef3dfa8584b
jihyunniiii <jita1125@naver.com>
2024-04-05 04:53:04 +0900

    [feat] #1 app 모듈

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.sopt.android.application)
+    alias(libs.plugins.sopt.android.test)
+    alias(libs.plugins.sopt.android.hilt)
+}
+
+android {
+    namespace = "com.sopt.now"
+
+    defaultConfig {
+        applicationId = "com.sopt.now"
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        dataBinding = true
+        buildConfig = true
+    }
+}
