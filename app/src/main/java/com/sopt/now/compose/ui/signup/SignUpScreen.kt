69f5f0dce24cb918b78257f4778628be93d16969
jihyunniiii <jita1125@naver.com>
2024-04-05 22:01:14 +0900

    [feat] #3 signUp

--- app/src/main/java/com/sopt/now/compose/ui/signup/SignUpScreen.kt
+++ app/src/main/java/com/sopt/now/compose/ui/signup/SignUpScreen.kt
@@ -1,5 +1,8 @@
 package com.sopt.now.compose.ui.signup
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,22 +16,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sopt.now.compose.ui.component.SoptButton
 import com.sopt.now.compose.ui.component.SoptTextField
+import com.sopt.now.compose.ui.model.UserModel
+import com.sopt.now.compose.ui.signin.SignInActivity
+import com.sopt.now.compose.ui.signin.SignInActivity.Companion.USER_INFO
 import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
+import com.sopt.now.compose.util.context.showToast
 
 @Composable
-fun SignUpScreen(
-    onClickSignUpBtn: () -> Unit = {}
-) {
+fun SignUpScreen() {
     var id by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var nickname by remember { mutableStateOf("") }
     var mbti by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -46,31 +53,62 @@ fun SignUpScreen(
         Spacer(modifier = Modifier.height(40.dp))
         SoptTextField(
             title = "ID",
+            value = id,
             onValueChange = { id = it },
             placeholder = "ID를 입력해 주세요"
         )
         Spacer(modifier = Modifier.height(20.dp))
         SoptTextField(
             title = "비밀번호",
+            value = password,
             onValueChange = { password = it },
             placeholder = "비밀번호를 입력해 주세요"
         )
         Spacer(modifier = Modifier.height(20.dp))
         SoptTextField(
             title = "닉네임",
+            value = nickname,
             onValueChange = { nickname = it },
             placeholder = "닉네임을 입력해 주세요"
         )
         Spacer(modifier = Modifier.height(20.dp))
         SoptTextField(
             title = "MBTI",
-            onValueChange = { password = it },
+            value = mbti,
+            onValueChange = { mbti = it },
             placeholder = "MBTI를 입력해 주세요"
         )
         Spacer(modifier = Modifier.weight(1f))
         SoptButton(
             text = "회원가입",
-            onClick = onClickSignUpBtn
+            onClick = {
+                context.showToast(
+                    when {
+                        id.length !in 6..10 -> "아이디를 확인해 주세요."
+                        password.length !in 8..12 -> "비밀번호를 확인해 주세요."
+                        nickname.isEmpty() -> "닉네임을 확인해 주세요."
+                        !mbti.matches(Regex("^[EI][NS][FT][JP]\$")) -> "MBTI를 확인해 주세요"
+                        else -> {
+                            Intent(context, SignInActivity::class.java).apply {
+                                putExtra(
+                                    USER_INFO,
+                                    UserModel(
+                                        id = id,
+                                        password = password,
+                                        nickname = nickname,
+                                        mbti = mbti
+                                    )
+                                )
+                                (context as? Activity)?.let { activity ->
+                                    activity.setResult(RESULT_OK, this)
+                                    activity.finish()
+                                }
+                            }
+                            "회원가입에 성공하였습니다"
+                        }
+                    }
+                )
+            }
         )
         Spacer(modifier = Modifier.height(24.dp))
     }
