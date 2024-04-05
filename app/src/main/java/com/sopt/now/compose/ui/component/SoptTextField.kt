56825a58ff4524b6c11db02e15f0d18dcc36908f
jihyunniiii <jita1125@naver.com>
2024-04-05 22:54:22 +0900

    [feat] #3 SoptTextField visualTransformation

--- app/src/main/java/com/sopt/now/compose/ui/component/SoptTextField.kt
+++ app/src/main/java/com/sopt/now/compose/ui/component/SoptTextField.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,7 +28,8 @@ fun SoptTextField(
     placeholder: String = "",
     title: String = "",
     value: String = "",
-    onValueChange: (String) -> Unit = { _ -> }
+    onValueChange: (String) -> Unit = { _ -> },
+    visualTransformation: VisualTransformation = VisualTransformation.None
 ) {
     Column(
         modifier = modifier
@@ -41,28 +46,27 @@ fun SoptTextField(
         )
         Spacer(modifier = Modifier.padding(top = 4.dp))
         BasicTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = value,
             onValueChange = onValueChange,
-            modifier = Modifier.fillMaxWidth(),
-            maxLines = 1,
-            textStyle = TextStyle(fontSize = 18.sp, color = Color(0xFF262525), lineHeight = 23.sp)
-        ) {
-            if (value.isEmpty()) {
-                Text(
-                    text = placeholder,
-                    fontSize = 18.sp,
-                    color = Color(0xFFCBC8C5),
-                    lineHeight = 23.sp
-                )
-            } else {
-                Text(
-                    text = value,
-                    fontSize = 18.sp,
-                    color = Color(0xFF262525),
-                    lineHeight = 23.sp
-                )
+            cursorBrush = SolidColor(Color.Black),
+            singleLine = true,
+            keyboardActions = KeyboardActions.Default,
+            keyboardOptions = KeyboardOptions.Default,
+            visualTransformation = visualTransformation,
+            textStyle = TextStyle(fontSize = 18.sp, color = Color(0xFF262525), lineHeight = 23.sp),
+            decorationBox = { innerTextField ->
+                innerTextField()
+                if (value.isEmpty()) {
+                    Text(
+                        fontSize = 18.sp,
+                        color = Color(0xFFCBC8C5),
+                        lineHeight = 23.sp,
+                        text = placeholder
+                    )
+                }
             }
-        }
+        )
     }
 }
 
