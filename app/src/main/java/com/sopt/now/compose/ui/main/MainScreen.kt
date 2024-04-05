f6d88773b2390ebe36d33f73e1f67229580cf778
jihyunniiii <jita1125@naver.com>
2024-04-05 22:01:42 +0900

    [feat] #3 main

--- app/src/main/java/com/sopt/now/compose/ui/main/MainScreen.kt
+++ app/src/main/java/com/sopt/now/compose/ui/main/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.sopt.now.compose.ui.main
 
+import android.app.Activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -12,8 +13,13 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,12 +27,21 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sopt.now.compose.R
 import com.sopt.now.compose.ui.model.UserModel
+import com.sopt.now.compose.ui.signin.SignInActivity.Companion.USER_INFO
 import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
+import com.sopt.now.compose.util.intent.getCompatibleParcelableExtra
 
 @Composable
-fun MainScreen(
-    userModel: UserModel
-) {
+fun MainScreen() {
+    val context = LocalContext.current
+    var user by remember {
+        mutableStateOf(
+            (context as Activity).intent.getCompatibleParcelableExtra<UserModel>(
+                USER_INFO
+            ) ?: UserModel("", "", "", "")
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -52,14 +67,14 @@ fun MainScreen(
                     modifier = Modifier
                         .background(color = Color(0xFFFFCFC0), shape = RoundedCornerShape(12.dp))
                         .padding(vertical = 4.dp, horizontal = 10.dp),
-                    text = userModel.mbti,
+                    text = user.mbti,
                     fontSize = 12.sp,
                     lineHeight = 16.sp,
                     color = Color(0xFFF0683E)
                 )
                 Spacer(modifier = Modifier.height(6.dp))
                 Text(
-                    text = userModel.nickname,
+                    text = user.nickname,
                     fontSize = 20.sp,
                     lineHeight = 28.sp,
                     fontWeight = FontWeight.Bold,
@@ -67,7 +82,7 @@ fun MainScreen(
                 )
                 Spacer(modifier = Modifier.height(6.dp))
                 Text(
-                    text = "id : " + userModel.id,
+                    text = "id : " + user.id,
                     fontSize = 12.sp,
                     lineHeight = 16.sp,
                     color = Color(0xFF878784)
@@ -81,13 +96,6 @@ fun MainScreen(
 @Composable
 fun MainScreenPreview() {
     NOWSOPTAndroidTheme {
-        MainScreen(
-            userModel = UserModel(
-                id = "jihyunniiii",
-                password = "aaaa1111",
-                nickname = "지현",
-                mbti = "ESFJ"
-            )
-        )
+        MainScreen()
     }
 }
