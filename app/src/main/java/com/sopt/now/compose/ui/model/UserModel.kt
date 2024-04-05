800223ff11570e0bc47858f3842c19e919f4ce6d
jihyunniiii <jita1125@naver.com>
2024-04-05 22:00:18 +0900

    [chore] #3 UserModel Parcelable

--- app/src/main/java/com/sopt/now/compose/ui/model/UserModel.kt
+++ app/src/main/java/com/sopt/now/compose/ui/model/UserModel.kt
@@ -1,8 +1,12 @@
 package com.sopt.now.compose.ui.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class UserModel(
     val id: String,
     val password: String,
     val nickname: String,
     val mbti: String
-)
+): Parcelable
