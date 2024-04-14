fd692c3de0419e92157fd9f7465e395c33e5d21b
jihyunniiii <jita1125@naver.com>
2024-04-15 02:52:17 +0900

    [feat] #3 home

--- app/src/main/java/com/sopt/now/compose/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sopt/now/compose/ui/home/HomeScreen.kt
@@ -1,6 +1,5 @@
-package com.sopt.now.compose.ui.main
+package com.sopt.now.compose.ui.home
 
-import android.app.Activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -13,34 +12,49 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.sopt.now.compose.R
-import com.sopt.now.compose.ui.model.UserModel
-import com.sopt.now.compose.ui.signin.SignInActivity.Companion.USER_INFO
-import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
-import com.sopt.now.compose.util.intent.getCompatibleParcelableExtra
+import com.sopt.now.compose.model.UserModel
+import com.sopt.now.compose.theme.NOWSOPTAndroidTheme
 
 @Composable
-fun MainScreen() {
-    val context = LocalContext.current
-    var user by remember {
-        mutableStateOf(
-            (context as Activity).intent.getCompatibleParcelableExtra<UserModel>(
-                USER_INFO
-            ) ?: UserModel("", "", "", "")
-        )
+fun HomeRoute(
+    navController: NavController,
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(true) {
+        navController.previousBackStackEntry?.savedStateHandle?.run {
+            get<UserModel>("user")?.let { userModel: UserModel ->
+                viewModel.setUserInfo(userModel = userModel)
+            }
+        }
     }
+
+    HomeScreen(
+        state = uiState
+    )
+}
+
+@Composable
+fun HomeScreen(
+    state: HomeContract.HomeState = HomeContract.HomeState()
+) {
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -49,7 +63,7 @@ fun MainScreen() {
     ) {
         Spacer(modifier = Modifier.height(60.dp))
         Text(
-            text = "마이페이지",
+            text = context.getString(R.string.my_page_my_page),
             color = Color(0xFF000000),
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
@@ -63,26 +77,33 @@ fun MainScreen() {
             )
             Spacer(modifier = Modifier.width(20.dp))
             Column {
-                Text(
-                    modifier = Modifier
-                        .background(color = Color(0xFFFFCFC0), shape = RoundedCornerShape(12.dp))
-                        .padding(vertical = 4.dp, horizontal = 10.dp),
-                    text = user.mbti,
-                    fontSize = 12.sp,
-                    lineHeight = 16.sp,
-                    color = Color(0xFFF0683E)
-                )
+                state.user?.let {
+                    Text(
+                        modifier = Modifier
+                            .background(
+                                color = Color(0xFFFFCFC0),
+                                shape = RoundedCornerShape(12.dp)
+                            )
+                            .padding(vertical = 4.dp, horizontal = 10.dp),
+                        text = it.mbti,
+                        fontSize = 12.sp,
+                        lineHeight = 16.sp,
+                        color = Color(0xFFF0683E)
+                    )
+                }
+                Spacer(modifier = Modifier.height(6.dp))
+                state.user?.let {
+                    Text(
+                        text = it.nickname,
+                        fontSize = 20.sp,
+                        lineHeight = 28.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF3D3A3A)
+                    )
+                }
                 Spacer(modifier = Modifier.height(6.dp))
                 Text(
-                    text = user.nickname,
-                    fontSize = 20.sp,
-                    lineHeight = 28.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color(0xFF3D3A3A)
-                )
-                Spacer(modifier = Modifier.height(6.dp))
-                Text(
-                    text = "id : " + user.id,
+                    text = "id : " + (state.user?.id ?: ""),
                     fontSize = 12.sp,
                     lineHeight = 16.sp,
                     color = Color(0xFF878784)
@@ -96,6 +117,6 @@ fun MainScreen() {
 @Composable
 fun MainScreenPreview() {
     NOWSOPTAndroidTheme {
-        MainScreen()
+        HomeScreen()
     }
 }
---
+++ app/src/main/java/com/sopt/now/compose/ui/home/HomeViewModel.kt
@@ -0,0 +1,19 @@
+package com.sopt.now.compose.ui.home
+
+import com.sopt.now.compose.model.UserModel
+import com.sopt.now.compose.util.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : BaseViewModel<HomeContract.HomeState, HomeContract.HomeSideEffect, HomeContract.HomeEvent>(){
+    override fun createInitialState(): HomeContract.HomeState = HomeContract.HomeState()
+
+    override suspend fun handleEvent(event: HomeContract.HomeEvent) {
+
+    }
+
+    fun setUserInfo(userModel: UserModel) {
+        setState { currentState.copy(user = userModel) }
+    }
+}
