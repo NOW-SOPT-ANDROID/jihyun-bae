600edfb194a32c3cd5f2893791ddff46ed3e4034
jihyunniiii <jita1125@naver.com>
2024-04-15 02:52:09 +0900

    [feat] #3 Main navigation

--- app/src/main/java/com/sopt/now/compose/ui/navigator/MainActivity.kt
+++ app/src/main/java/com/sopt/now/compose/ui/navigator/MainActivity.kt
@@ -1,14 +1,19 @@
-package com.sopt.now.compose.ui.main
+package com.sopt.now.compose.ui.navigator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.now.compose.theme.NOWSOPTAndroidTheme
+import com.sopt.now.compose.ui.home.HomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +27,13 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainPreview() {
+    NOWSOPTAndroidTheme {
+        MainScreen()
     }
 }
