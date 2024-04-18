1a61c03feb21446155bfb8a8c9e5aa4c0aa9a949
jihyunniiii <jita1125@naver.com>
2024-04-19 01:11:55 +0900

    [chore] #22 불필요한 import 제거

--- app/src/main/java/com/sopt/now/compose/presentation/ui/home/HomeHeaderContainer.kt
+++ app/src/main/java/com/sopt/now/compose/presentation/ui/home/HomeHeaderContainer.kt
@@ -1,6 +1,5 @@
 package com.sopt.now.compose.presentation.ui.home
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,12 +7,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.sopt.now.compose.R
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeStickyHeader(
     title: String
