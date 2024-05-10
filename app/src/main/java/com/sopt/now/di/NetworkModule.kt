b1847a58e321ecf2ad2cddd6c2215d593ca88a12
jihyunniiii <jita1125@naver.com>
2024-05-10 16:54:22 +0900

    [chore] #26 네이밍 변경 (RetrofitModule -> NetworkModule)

--- app/src/main/java/com/sopt/now/di/NetworkModule.kt
+++ app/src/main/java/com/sopt/now/di/NetworkModule.kt
@@ -23,7 +23,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RetrofitModule {
+object NetworkModule {
     @OptIn(ExperimentalSerializationApi::class)
     @Provides
     @Singleton
