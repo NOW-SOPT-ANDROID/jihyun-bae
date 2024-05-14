1475970b75d7e7b3c0e4918755b574c07389177b
jihyunniiii <jita1125@naver.com>
2024-05-14 18:23:59 +0900

    [chore] #28 Qualifier 리텐션 제거

--- app/src/main/java/com/sopt/now/compose/di/qualifier/Qualifier.kt
+++ app/src/main/java/com/sopt/now/compose/di/qualifier/Qualifier.kt
@@ -3,9 +3,7 @@ package com.sopt.now.compose.di.qualifier
 import javax.inject.Qualifier
 
 @Qualifier
-@Retention(AnnotationRetention.BINARY)
 annotation class Sopt
 
 @Qualifier
-@Retention(AnnotationRetention.BINARY)
 annotation class Reqres
